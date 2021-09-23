627d80b3b0335d1dad1218589e15f357f65bf2ec
Caroline Kuria <kuriacarol8@gmail.com>
2021-09-23 08:23:19 +0300

    Initial Commit

---
+++ app/src/main/java/com/kuria/babybasket/AdminCategoryActivity.java
@@ -0,0 +1,193 @@
+package com.kuria.babybasket;
+
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.view.View;
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminCategoryActivity extends AppCompatActivity {
+    private ImageView tShirts, gtop, femaleDresses, overall;
+    private ImageView bottie, hatsCaps, diapers, shoes;
+    private ImageView babycrib, dungarees, shorts, stroller;
+    private Button LogoutBtn, CheckOrdersBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_category);
+
+        LogoutBtn = (Button) findViewById(R.id.admin_logout_btn);
+        LogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(AdminCategoryActivity.this,MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+        CheckOrdersBtn = (Button) findViewById(R.id.check_orders_btn);
+
+
+        CheckOrdersBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(AdminCategoryActivity.this,AdminNewOrdersActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        tShirts = (ImageView) findViewById(R.id.t_shirts);
+        gtop = (ImageView) findViewById(R.id.gtop);
+        femaleDresses = (ImageView) findViewById(R.id.female_dresses);
+        overall = (ImageView) findViewById(R.id.overall);
+
+        bottie = (ImageView) findViewById(R.id.bottie);
+        hatsCaps = (ImageView) findViewById(R.id.hats_caps);
+        diapers = (ImageView) findViewById(R.id.diapers);
+        shoes = (ImageView) findViewById(R.id.shoes);
+
+        babycrib = (ImageView) findViewById(R.id.babycrib);
+        dungarees = (ImageView) findViewById(R.id.dungarees);
+        shorts = (ImageView) findViewById(R.id.shorts);
+        stroller = (ImageView) findViewById(R.id.stroller);
+
+
+        tShirts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "tShirts");
+                startActivity(intent);
+            }
+        });
+        gtop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Girl Tops");
+                startActivity(intent);
+            }
+        });
+
+
+        femaleDresses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Female Dresses");
+                startActivity(intent);
+            }
+        });
+
+
+        overall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Overall");
+                startActivity(intent);
+            }
+        });
+
+
+        bottie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Bottie");
+                startActivity(intent);
+            }
+        });
+
+
+        hatsCaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Hats Caps");
+                startActivity(intent);
+            }
+        });
+
+
+
+        diapers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Diapers");
+                startActivity(intent);
+            }
+        });
+
+
+        shoes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Shoes");
+                startActivity(intent);
+            }
+        });
+
+
+
+        babycrib.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "BabyCrib");
+                startActivity(intent);
+            }
+        });
+
+
+        dungarees.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Dungarees");
+                startActivity(intent);
+            }
+        });
+
+
+        shorts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Shorts");
+                startActivity(intent);
+            }
+        });
+
+
+        stroller.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Stroller");
+                startActivity(intent);
+            }
+        });
+    }
+}
